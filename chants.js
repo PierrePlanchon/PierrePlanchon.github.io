0c9ec285dafc90c1f050938a445bd6577635fd1d
PierrePlanchon <119939663+PierrePlanchon@users.noreply.github.com>
2025-01-16 20:57:38 +0100

    site fonctionnel + ajout chants

--- chants.js
+++ chants.js
@@ -1,30 +1,215 @@
 const chantsData = [
-    {
-      "titre": "Bougalou",
-      "paroles": "\n1, 2, 3, 4 ! 5, 6, 7, 8 (bis)\nMais montre moi ton Bougalou !\nMais c'est quoi un Bougalou ? (bis)\nBou ! Galou ! Bou Bou Galou, wouh ! (bis)",
-      "age": "3-6",
-      "intensite": 1,
-      "complexite": 1,
-    },
-    {
-      "titre": "Le grand serpent de la montagne",
-      "paroles": "\nJe suis le grand serpent, le grand serpent de la montagne\nEt je ne suis pas content car j'ai perdu ma queue !\nToi qui est mon ami, ne veux-tu pas, oui, toi aussi ?\nEtre un p'tit bout d'ma queue, un tout p'tit bout d'ma queue ?",
-      "age": "6-10",
-      "intensite": 1,
-      "complexite": 2,
-    },
-    {
-      "titre": "Piston",
-      "paroles": "\nEt c'est l'piston, piston, piston qui fait avancer la machine\nEt c'est l'piston, piston, piston qui fait avancer les wagons !\nPiston-ON ! Piston-ON !\nQui fait avancer la machine\nPiston-ON ! Piston-ON !\nQui fait avancer les wagons !",
-      "age": "11-14",
-      "intensite": 3,
-      "complexite": 1,
-    },
-    {
-      "titre": "Egyptum",
-      "paroles": "\nEgyptum, caraboum, tic et tic et tic et aïe aïe aïe !\nDans les dunes, je déambule,\nC'est la chanson de tout-en-carton !",
-      "age": "3-6",
-      "intensite": 1,
-      "complexite": 1,
-    }
-  ]
+  {
+    "titre": "Bougalou",
+    "paroles": "\nMeneur : 1, 2, 3, 4 ! \nGroupe : 5, 6, 7, 8 !\nMeneur : Mais montre moi ton Bougalou !\nGroupe : Mais c'est quoi un Bougalou ?\nTous : Bou ! Galou ! Bou Bou Galou, wouh !\n\nRépéter ce schéma en remplaçant Bougalou par \nSpiderman, Reine des neiges, Frankenstein, Top Model, un animateur, ...",
+    "age": "6-10",
+    "intensite": 2,
+    "complexite": 1,
+  },
+  {
+    "titre": "Le grand serpent de la montagne",
+    "paroles": "\nJe suis le grand serpent, le grand serpent de la montagne\nEt je ne suis pas content car j'ai perdu ma queue !\nToi qui est mon ami, ne veux-tu pas, oui, toi aussi ?\nEtre un p'tit bout d'ma queue, un tout p'tit bout d'ma queue ?",
+    "age": "6-10",
+    "intensite": 2,
+    "complexite": 1,
+  },
+  {
+    "titre": "Piston",
+    "paroles": "\nEt c'est l'piston, piston, piston qui fait avancer la machine\nEt c'est l'piston, piston, piston qui fait avancer les wagons !\nPiston-ON ! Piston-ON !\nQui fait avancer la machine\nPiston-ON ! Piston-ON !\nQui fait avancer les wagons !",
+    "age": "11-14",
+    "intensite": 3,
+    "complexite": 1,
+  },
+  {
+    "titre": "Egyptum",
+    "paroles": "\nEgyptum, caraboum, tic et tic et tic et aïe aïe aïe !\nDans les dunes, je déambule,\nC'est la chanson de tout-en-carton !\n\nRecommencer en remlaçant tout-en-carton par tout-en-calecon,\n tout-en-camion puis Toutankhamon.",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1,
+  },
+  {
+    "titre": "Une araignée",
+    "paroles": "Une araignée sur le plancher se tricotait des bottes.\nDans un flacon un limaçon enfilait sa culotte.\nJ'ai vu dans le ciel une mouche à miel pinçant sa guitare.\nLes rats tout confus sonnaient l'angelus au son de la fanfarre.",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1,
+  },
+  {
+    "titre": "Ma tante au Maroc",
+    "paroles": "\nJ'ai une tante qui est au Maroc et qui s'appelle Hip Hop !\nJ’ai une tante qui est au Maroc\nJ’ai une tante qui est au Maroc et qui s'appelle Hip Hop !\n\nElle traverse le désert en dromadaire... Houla houla... Hip Hop !\nElle traverse le désert\nElle traverse le désert en dromadaire... Houla houla... Hip Hop !\n\nEt elle boit d’la grenadine quand elle a soif... Glou glou... Houla houla... Hip Hop !\nEt elle boit d’la grenadine\nEt elle boit d’la grenadine quand elle a soif... Glou glou... Houla houla... Hip Hop !\n\nEt elle mange du chocolat quand elle a faim... Miam miam... Glou glou... Houla houla... Hip Hop !\nEt elle mange du chocolat\nEt elle mange du chocolat quand elle a faim... Miam miam... Glou glou... Houla houla... Hip Hop !\n\nEt elle met une mini-jupe quand elle a chaud... Mini Mini... Miam miam... Glou glou... Houla houla... Hip Hop !\nEt elle met une mini-jupe\nEt elle met ne mini-jupe quand elle a chaud... Mini Mini... Miam miam... Glou glou... Houla houla... Hip Hop !\n\nEt elle tire au pistolet quand elle a peur... Pan Pan... Mini Mini... Miam miam... Glou glou... Houla houla... Hip Hop !\nEt elle tire au pistolet\nEt elle tire au pistolet quand elle a peur... Pan Pan... Mini Mini... Miam miam... Glou glou... Houla houla... Hip Hop !",
+    "age": "6-10",
+    "intensite": 2,
+    "complexite": 1,
+  },
+  {
+    "titre": "Je suis un artiste",
+    "paroles": "\nMeneur : Je suis un artiste\nEt je reviens de Paris.\nGroupe : Nous sommes des artistes\nEt nous revenons de Paris.\nMeneur : Et moi je sais jouer,\nGroupe : Et nous savons jouer,\nMeneur : De la clarinette.\nGroupe : De la clarinette.\nTous : Cla, cla, cla, cla, cla, cla,\nCla, cla, cla, cla, cla.\n\nMeneur : Je suis un artiste\nEt je reviens de Paris.\nGroupe : Nous sommes des artistes\nEt nous revenons de Paris.\nMeneur : Et moi je sais jouer,\nGroupe : Et nous savons jouer,\nMeneur : De la pianinette.\nGroupe : De la pianinette.\nTous : Pia, pia, pia, pia, pia, pia,\nPia, pia, pia, pia, pia.\nCla, cla, cla, cla, cla, cla,\nCla, cla, cla, cla, cla.\n\nRépéter ce schéma en rajoutant des instruments.",
+    "age": "6-10",
+    "intensite": 2,
+    "complexite": 1,
+  },
+  {
+    "titre": "Oh! Ursule",
+    "paroles": "\nOh! U! Oh! Ursule!\nPour toi d'amour mon coeur brûle!\nIl faudrait, il faudrait\nune pompe à vapeur\nPour éteindre le feu\nqui consume mon coeur\n\nJ'aime tes grands yeux\nderrière tes lunettes\nIls me font penser\naux phares de ma camionnette\n\nREFRAIN\n\nJ'aime ton grand nez\nen forme de lucarne\nSi tu te mouchais\nça ferait tout un vacarme\n\nREFRAIN\n\nJ'aime tes grands pieds\nqui sentent le fromage\nSi tu les lavais,\nce serait bien dommage\n\nREFRAIN\n\nJ'aime tes cheveux\nen forme de tignasse\nIls me font penser\naux poils de ma vache\n\nREFRAIN\n\nJ'aime tes grandes oreilles\nen forme de porte de grange\nElles me font penser\naux ailes d’un ange\n\nREFRAIN\n\nJ'aime tes grandes dents\nsi jaunes et si vertes\nSi tu les lavais...\nAh! Quelle découverte!\n\nREFRAIN\n\nJ'aime tes boutons\nau milieu de ta face\nIls me font penser\naux fraises dans ma tarte\n\nREFRAIN\n\nJ'aime tes sourcils\nen forme de faucille\nIls me font penser\nà de grosses chenilles\n\nREFRAIN\n\nJ'aime ta grande bouche\nen forme de zig-zag\nElle me fait penser\nà la mer et ses vagues\n\nREFRAIN\n\nJ'aime ton nombril\ntout plein de mottes de terre\nSi tu le grattais\nça ferait tout un cratère",
+    "age": "6-10",
+    "intensite": 2,
+    "complexite": 1,
+  },
+  {
+    "titre": "A l'intérieur d'une citrouille",
+    "paroles": "\nA l'intérieur d'une citrouille\nVivait un papillon géant\nFort amoureux d'une grenouille\nQui se plaignait d'un mal de dent\n\nOh là là c'que j'ai mal aux dents\nOh la coquine, la coquine, la coquine\nOh là là c'que j'ai mal aux dents\nOh la coquine, la coquine de dent",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1,
+  },
+  {
+    "titre": "Les petites grenouilles",
+    "paroles": "\nHum Hum font les petites grenouilles\nMais elles ne font pas \nHum Hum Haa! \nLes grenouilles font\nChabadabada, \nMais elles ne font pas\nHum Hum Haa!",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1,
+  },
+  {
+    "titre": "C'est un petit bonhomme",
+    "paroles": "\nC'est un petit bonhomme, petit, petit, petit…\nSa tête est une pomme\nSon nez est un radis\nSes yeux sont des groseilles\nSa bouche est un bonbon\nEt il a comme oreilles\ndes graines de melon, lon, lon, lon...\n\nSa jambe est une banane\nSon autre jambe aussi\nÀ sa main, une canne en sucre de candi\nIl a une longue barbe\nUn tout petit chapeau\nEt une feuille de rhubarbe\nLui sert de long manteau, teau, teau, teau.",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1
+  },
+  {
+    "titre": "C'est un petit clown",
+    "paroles": "\nC'est un petit clown, clown, clown, clown, clown,\nQui fait du vélo, qui qui fait du vélo,\nC'est un petit clown, clown, clown, clown, clown,\nQui fait du vélo sur son joli p'tit fil doré.\n\nEst-ce qu'il va tomber, tomber, tomber,\nCe tout petit clown, ce tout petit clown ?\nEst-ce qu'il va tomber, tomber, tomber,\nCe tout petit clown sur son joli p'tit fil doré ?",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1
+  },
+  {
+    "titre": "J'ai un gros nez rouge",
+    "paroles": "\nJ'ai un gros nez rouge\nDeux traits sous les yeux\nUn chapeau qui bouge\nUn air malicieux\nDeux grandes savates\nUn grand pantalon\nEt quand ça me gratte\nJe saute au plafond.",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1
+  },
+  {
+    "titre": "Il y a un rat sous mon toit",
+    "paroles": "\nIl y a un rat sous mon toit\nJ'entends le chat qui miaule\nIl y a un rat sous mon toit\nJ'entends le chat miauler\n\nJ'entends miaou j'entends miaou\nJ'entends le chat qui miaule\nJ'entends miaou j'entends miaou\nJ'entends le chat miauler",
+    "age": "3-6",
+    "intensite": 2,
+    "complexite": 1
+  },
+  {
+    "titre": "Une bulle bulle vole",
+    "paroles": "\nUne bulle bulle vole\nQuatre bulles bulles s’envolent\nDes milliers de bulles font la farandole\nUne bulle bulle vole",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1
+  },
+  {
+    "titre": "C'est la baleine",
+    "paroles": "\nC'est la baleine\nqui tourne qui vire\ncomme un joli petit navire\nPrenez garde à vos p'tits doigts\nou la baleine les mangera !\nMiam !\n\nEnlever un doigt et répéter jusqu'à n'avoir plus aucun doigt.",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1
+  },
+  {
+    "titre": "Un éléphant",
+    "paroles": "\nUn éléphant\nQui se baladait\nTout doucement\nDans la forêt\n\nIl avait sur son dos\nUn petit perroquet\nQui s'appelait Jaquot\nEt qui buvait du lait.\n\nRecommencer en remplaçant toutes les voyelles par e , a, i, o, u",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1
+  },
+  {
+    "titre": "On pagaie, on pagaie",
+    "paroles": "\nOn pagaie, on pagaie\nElle est où la pagaie ?\nLà bas sous les cocotiers\nC'est l'croco qui l'a mangée\nEt on peut plus pagayer",
+    "age": "3-6",
+    "intensite": 2,
+    "complexite": 1
+  },
+  {
+    "titre": "Hier au soir",
+    "paroles": "\nHier au soir\nTa pomme ta poire\nUne jeune fille\nTa pomme ta poire\nfut assassinée\nTa pomme ta poire\nPar une bande de bandits\n\nEt la police\nTa pomme ta poire\nToujours pleine de malice\nTa pomme ta poire\nfut poser une affiche\nTa pomme ta poire\nSur laquelle on lisait\n\nC'est la mère Michelle qui a perdu son chat\nQui crie par la fenêtre\nAux armes citoyens mon oncle Julien\nFormez vos bataillons mon oncle Léon\nMarchons, marchons\n\nNon je ne marcherais pas\nCar ils étaient 4\nà vouloir se battre\nContre 3\nMais le 4ème dit\n\nMalbrougt s'en va en guerre\nHaricot champignon pomme de terre\nMalbrougt s'en va en guerre\nSur un petit, petit navire\nQui n'avait ja-ja-jamais on a vu-vu-vu\nJamais on n'verra-ra-ra\nLa queue d'une souri-ri-ris\nDans l'oreille d'un chat-chat-chat",
+    "age": "11-14",
+    "intensite": 1,
+    "complexite": 3
+  },
+  {
+    "titre": "Brousse, brousse",
+    "paroles": "\nBrousse, brousse\nJ'aime la brousse\nJ'aime la brousse et la jolie savane\nY'a des tigres et y'a des lions\nY'a des léopards\nJ'aime la brousse et la jolie savane",
+    "age": "6-10",
+    "intensite": 3,
+    "complexite": 1
+  },
+  {
+    "titre": "Lucky Luke",
+    "paroles": "\nSur son cheval blanc couvert de poussière\nIl va galopant d'une allure fière\nRoi du pistolet et roi du lasso\nDe tout le Far-West c'est lui le héros\n\nC'est Lucky Luke\nLucky Luke\nLucky Luke\nTa tagada tagada tagada\n\nAttention Lucky, voilà les Dalton\nPrépare ton fusil et crache ton chewing-gum\n4 coups de pétards claquent dans la nuit\n1 ! 2 ! 3 ! 4 !\nLes Daltons sont cuits.",
+    "age": "6-10",
+    "intensite": 2,
+    "complexite": 1
+  },
+  {
+    "titre": "Les bidons d'eau",
+    "paroles": "\n1 bidon, 2 bidons, 3 bidons d'eau\n4 bidons d'eau, 5 bidons d'eau\n6 bidons, 7 bidons, 8 bidons d'eau\n9 et 10 bidons d'eau\n\n10 bidons, 9 bidons, 8 bidons d'eau,\n7 bidons d'eau, 6 bidons d'eau,\n5 bidons, 4 bidons, 3 bidons d'eau,\n2 et 1 bidon d'eau",
+    "age": "6-10",
+    "intensite": 1,
+    "complexite": 2
+  },
+  {
+    "titre": "La famille tortue",
+    "paroles": "\nJamais on a vu\nJamais on ne verra\nLa famille tortue courir après les rats\nLe papa tortue et la maman tortue\nEt les enfants tortue\nIront toujours au pas !",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1
+  },
+  {
+    "titre": "La maison de ma tante",
+    "paroles": "\nC'était ma tante qui avait une maison !\nC'était la maison de ma tante !\n\nDans cette maison, y'avait une allée !\nC'était l'allée de la maison de ma tante !\n\nDans cette allée, y'avait un jardin !\nC'était le jardin de l'allée de la maison de ma tante !\n\nDans ce jardin, y'avait un pommier !\nC'était le pommier du jardin de l'allée de la maison de ma tante !\n\nSur ce pommier, y'avait une pomme !\nC'était la pomme du pommier du jardin de l'allée de la maison de ma tante !\n\nSur cette pomme, y'avait un oiseau !\nC'était l'oiseau de la pomme du pommier du jardin de l'allée de la maison de ma tante !\n\nSur cet oiseau, y'avait une plume !\nC'était la plume de l'oiseau de la pomme du pommier du jardin de l'allée de la maison de ma tante !\n\nAvec cette plume, on fit un matelas !\nC'était le matelas de la plume de l'oiseau de la pomme du pommier du jardin de l'allée de la maison de ma tante !",
+    "age": "6-10",
+    "intensite": 2,
+    "complexite": 3
+  },
+  {
+    "titre": "Derrière chez moi",
+    "paroles": "\nDerrière chez moi, devinez quoi qu'il y a ?\nY a un arbre, le plus beau des arbres\nArbre dans le bois\n\nRefrain :\nEhh petit bois derrière chez moi\nTraloula loulere et traloula loula\nTraloula loulere et traloula loula\n\nEt dans cet arbre devinez quoi qu'il y a ?\nY a une branche, la plus grande des branches\nBranche dans l'arbre, arbre dans le bois\n\nRefrain\n\nEt sur cette branche devinez quoi qu'il y a ?\nY a un nid, un tout p'tit nid\nNid sur la branche, branche dans l'arbre, arbre dans le bois\n\nRefrain\n\nEt dans ce nid devinez quoi qu'il y a ?\nY a un oeuf, un tout p'tit oeuf\nOeuf dans le nid, nid sur la branche, ...\n\nRefrain\n\nEt dans cet oeuf devinez quoi qu'il y a ?\nY a un zozio, un tout p'tit zozio\nZozio dans l'oeuf, oeuf dans le nid, ...\n\nRefrain\n\nEt dans le zozio devinez quoi qu'il y a ?\nY a un coeur, un tout p'tit coeur\nCoeur dans le zozio, zozio dans l'oeuf, ...\n\nRefrain",
+    "age": "6-10",
+    "intensite": 2,
+    "complexite": 2
+  },
+  {
+    "titre": "A Monaco",
+    "paroles": "\nA Monaco c'est magnifique-fique\nLes soldats sont très sympathiques-tiques\nQuand ils boivent du cacolac-lac\nLeur coeur fait tic tic tac\ntic tac, tic tic tac tac\ntic tac, tic tic tac tac\ntic tac, tic tic tac tac\nLeur coeur fait tic tic tac\n\nPouce droit vers la droite à chaque terminaisons en IC\nPouce gauche vers la gauche à chaque terminaisons en AC",
+    "age": "6-10",
+    "intensite": 1,
+    "complexite": 2
+  },
+  {
+    "titre": "Qui peut faire de la voile sans vent",
+    "paroles": "\nQui peut faire de la voile sans vent\nQui peut ramer sans rames\nEt qui peut quitter son ami\nSans verser de larmes\nJe peux faire de la voile sans vent\nJe peux ramer sans rames\nMais ne peux quitter mon ami\nSans verser de larmes\n\nQui peut faire du pain sans levain\nQui peut faire du vin sans raisin\nEt qui peut quitter son ami\nSans verser de larmes\nJe peux faire du pain sans levain\nJe peux faire du vin sans raisin\nMais ne peux quitter mon ami\nSans verser de larmes\n\nQui peut voir le soleil la nuit\nQui peut voir la nuit sans étoiles\nEt qui peut quitter son ami\nSans verser de larmes\nJe peux voir le soleil la nuit\nJe peux voir la nuit sans étoiles\nMais ne peux quitter mon ami\nSans verser de larmes",
+    "age": "6-10",
+    "intensite": 1,
+    "complexite": 1
+  },
+  {
+    "titre": "Savez-vous planter les choux",
+    "paroles": "\nSavez-vous planter les choux\nÀ la mode, à la mode,\nSavez-vous planter les choux\nÀ la mode, à la mode de chez vous.\n\nOn les plante avec le doigt,\nÀ la mode, à la mode,\nOn les plante avec le doigt,\nÀ la mode, à la mode de chez nous.\n\nOn les plante avec la main,\nÀ la mode, à la mode,\nOn les plante avec la main,\nÀ la mode, à la mode de chez nous.\n\nOn les plante avec la tête,\nÀ la mode, à la mode,\nOn les plante avec la tête,\nÀ la mode, à la mode de chez nous.\n\nBouger la partie du corps concernée.\nContinuer en changeant cette dernière à chaque fois",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 1
+  },
+  {
+    "titre": "Epo é taï taï é",
+    "paroles": "\nEpo é taï taï é\nEpo é taï taï é\nEpo é taï taï\nEpo é touki touki\nEpo é touki touki é",
+    "age": "3-6",
+    "intensite": 1,
+    "complexite": 2
+  },
+  {
+    "titre": "Les coureurs du Tour de France",
+    "paroles": "\nC’est nous les coureurs du tour de Fran-ance\nD’une ville à l’autre, on s’élance-ance\nLes gens émerveillés, nous regardent passer\nCousin Mathieu, Ma tante Zoé\nC’est nous les coureurs du tour de Fran-ance\n\nChanter lentement quand on imagine les coureurs prendre une côte et très vite quand ils descendent un col",
+    "age": "6-10",
+    "intensite": 3,
+    "complexite": 1
+  }
+  
+  
+  
+]
